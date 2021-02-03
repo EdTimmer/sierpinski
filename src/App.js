76d95b5b528226c32a662d560efe87f2a1a3b042
Ed Timmer <edtimmer@gmail.com>
2021-02-02 21:34:43 -0500

    can have square or triangle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Canvas from './Canvas';
 
-function App() {
+const App = () => {
+
+
+  const [shownDivider, setShownDivider] = useState(2);
+  const [divider, setDivider] = useState(2);
+  const [counter, setCounter] = useState(0);
+  const [shape, setShape] = useState('triangle');
+
+  const handleDividerChange = (e) => {
+    // console.log('handleDividerChange fired');
+    // console.log(typeof e.target.value);
+    e.preventDefault();
+    setShownDivider(e.target.value);
+    setDivider(e.target.value);
+    // console.log(divider);
+  };
+
+  const submitDivider = (e) => {
+    e.preventDefault();
+    // console.log('submitDivider fired');
+    setCounter(0);
+    setDivider(shownDivider);
+  };
+
+  const submitTriangle = (e) => {
+    e.preventDefault();
+    setShape('triangle');
+  };
+
+  const submitSquare = (e) => {
+    e.preventDefault();
+    setShape('square');
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitDivider}>
+        <label>
+          Divider:
+          <input
+            type="text"
+            value={shownDivider}
+            name="shownDivider"
+            onChange={handleDividerChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <button onClick={submitTriangle}>Triangle</button>
+      <button onClick={submitSquare}>Square</button>
+      <Canvas divider={divider} counter={counter} setCounter={setCounter} shape={shape} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Canvas.js
@@ -0,0 +1,152 @@
+import React, { useRef, useEffect, useState } from 'react';
+import './Canvas.css';
+
+const Canvas = ({ divider, counter, setCounter, shape }) => {
+  const canvasRef = useRef(null);
+
+  const [currentPoint, setCurrentPoint] = useState([30, 30]);
+
+
+  // const pointOne = [0, 500];
+  // const pointTwo = [250, 0];
+  // const pointThree = [500, 500];
+  // console.log('divider in Canvas: ', divider)
+
+
+  const dotSize = 2;
+
+  useEffect(() => {
+    const pointOneTriangle = [0, 500];
+    const pointTwoTriangle = [250, 0];
+    const pointThreeTriangle = [500, 500];
+
+    const pointOneSquare = [0, 0];
+    const pointTwoSquare = [500, 0];
+    const pointThreeSquare = [500, 500];
+    const pointFourSquare = [0, 500];
+
+    // const pointFour = [0, 250];
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    if (counter === 0) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    const shapePoints = [];
+    // draw start shape points
+    if (shape === 'triangle') {
+      context.fillRect(
+        pointOneTriangle[0],
+        pointOneTriangle[1],
+        dotSize,
+        dotSize
+      );
+      context.fillRect(
+        pointTwoTriangle[0],
+        pointTwoTriangle[1],
+        dotSize,
+        dotSize
+      );
+      context.fillRect(
+        pointThreeTriangle[0],
+        pointThreeTriangle[1],
+        dotSize,
+        dotSize
+      );
+
+      shapePoints.push(pointOneTriangle);
+      shapePoints.push(pointTwoTriangle);
+      shapePoints.push(pointThreeTriangle);
+    }
+
+    if (shape === 'square') {
+      context.fillRect(
+        pointOneSquare[0],
+        pointOneSquare[1],
+        dotSize,
+        dotSize
+      );
+      context.fillRect(
+        pointTwoSquare[0],
+        pointTwoSquare[1],
+        dotSize,
+        dotSize
+      );
+      context.fillRect(
+        pointThreeSquare[0],
+        pointThreeSquare[1],
+        dotSize,
+        dotSize
+      );
+      context.fillRect(
+        pointFourSquare[0],
+        pointFourSquare[1],
+        dotSize,
+        dotSize
+      );
+
+      shapePoints.push(pointOneSquare);
+      shapePoints.push(pointTwoSquare);
+      shapePoints.push(pointThreeSquare);
+      shapePoints.push(pointFourSquare);
+    }
+
+    // context.fillRect(pointFour[0], pointFour[1], dotSize, dotSize);
+
+    //select random shape point
+    // const shapePoints = [pointOne, pointTwo, pointThree];
+    const selectRandomPoint = (arr) => {
+      let randomPoint = arr[Math.floor(Math.random() * arr.length)];
+      return randomPoint;
+    };
+
+    // console.log('divider in useEffect: ', parseInt(divider));
+
+    const midwayPoint = (a, b) => {
+      // return (a + b) / 3;
+      return (a + b) / parseInt(divider);
+    };
+
+    const determineNewCurrentPoint = (currentPoint, randomPoint) => {
+      return [
+        midwayPoint(currentPoint[0], randomPoint[0]),
+        midwayPoint(currentPoint[1], randomPoint[1]),
+      ];
+    };
+
+    // for (let i = 0; i < 50; i++) {
+    //   console.log('inside for loop');
+    //   let randomPoint = selectRandomPoint(shapePoints);
+    //   setCurrentPoint(determineNewCurrentPoint(currentPoint, randomPoint));
+    //   context.fillRect(currentPoint[0], currentPoint[1], dotSize, dotSize);
+    //   setCounter(counter + 1);
+    // }
+
+    if (counter < 5000) {
+      let randomPoint = selectRandomPoint(shapePoints);
+      setCurrentPoint(determineNewCurrentPoint(currentPoint, randomPoint));
+      //
+      // context.fillRect(pointOne[0], pointOne[1], dotSize, dotSize);
+      // context.fillRect(pointTwo[0], pointTwo[1], dotSize, dotSize);
+      // context.fillRect(pointThree[0], pointThree[1], dotSize, dotSize);
+      context.fillRect(currentPoint[0], currentPoint[1], dotSize, dotSize);
+      setCounter(counter + 1);
+    }
+  }, [divider, counter]);
+
+
+
+  return (
+    <div className="">
+      <canvas
+        width="510"
+        height="510"
+        className="my-canvas"
+        ref={canvasRef}
+      />
+    </div>
+  );
+};
+
+export default Canvas;
